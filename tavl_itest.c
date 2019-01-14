25636871a8de4940bac3f06f30172e3dd9c98026
Timo Lassmann <timo.lassmann@telethonkids.org.au>
2019-01-13 20:11:57 -0500

    will switch to using libavl by Ben Pfaff

---
+++ tavl_itest.c
@@ -0,0 +1,12 @@
+
+#include "tldevel.h"
+#include "tavl.h"
+
+int main (int argc,char * argv[])
+{
+        fprintf(stdout,"Hello world\n");
+        return EXIT_SUCCESS;
+ERROR:
+        return EXIT_FAILURE;
+
+}
