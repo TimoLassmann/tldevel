5cc7cf731a9ce5f7132e1cf2969d2ed488c4bcf5
Timo Lassmann <timo.lassmann@telethonkids.org.au>
2019-05-20 23:51:43 +0200

    hash in tldevel

--- hash_table.c
+++ hash_table.c
@@ -1,7 +1,7 @@
 #include "hash_table.h"
 
 
-
+static uint32_t hash_uint32( uint32_t a);
 
 /* print commands */
 int print_int(const int a)
@@ -127,6 +127,16 @@ ERROR:
         return 0;
 }
 
+uint32_t hash_uint32( uint32_t a)
+{
+        a = (a+0x7ed55d16) + (a<<12);
+        a = (a^0xc761c23c) ^ (a>>19);
+        a = (a+0x165667b1) + (a<<5);
+        a = (a+0xd3a2646c) ^ (a<<9);
+        a = (a+0xfd7046c5) + (a<<3);
+        a = (a^0xb55a4f09) ^ (a>>16);
+        return a;
+}
 
 uint32_t get_hash_value_int_array(const int* x,const int table_size)
 {
@@ -135,10 +145,10 @@ uint32_t get_hash_value_int_array(const int* x,const int table_size)
         int len = DIM1(x);
         int i;
 
-        hash = hash ^ x[0];
+        hash = hash ^  hash_uint32(x[0]);
         for(i = 1; i < len;i++){
                 hash = (hash << 10) ^ ( hash >> (32 - 10));
-                hash ^= x[i];
+                hash ^= hash_uint32(x[i]);
         }
         return hash % table_size;
 ERROR:
