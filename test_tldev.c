cb5474e075ff0c34e30df9857cc697d6d08f21bc
Timo Lassmann <timo.lassmann@telethonkids.org.au>
2019-12-02 22:52:04 -0500

    Modular + better gfree gmalloc

---
+++ test_tldev.c
@@ -0,0 +1,39 @@
+
+#include "tldev.h"
+
+
+int main(int argc, char *argv[])
+{
+        int* blah = NULL;
+
+        RUNP(blah = galloc(blah,1));
+
+        gfree(blah);
+
+
+        LOG_MSG("%p",blah);
+        blah = NULL;
+        double** ga = NULL;
+
+        RUNP(ga = galloc(ga,19, 10));
+        LOG_MSG("d1: %d", get_dim1(ga));
+        LOG_MSG("d2: %d", get_dim2(ga));
+        RUNP(ga = galloc(ga,190, -100));
+        LOG_MSG("d1: %d", get_dim1(ga));
+        LOG_MSG("d2: %d", get_dim2(ga));
+        gfree(ga);
+        init_logsum();
+
+        float a,b;
+        a = prob2scaledprob(0.3f);
+        b = prob2scaledprob(0.3f);
+
+        LOG_MSG("SUM: %f", scaledprob2prob(logsum(a, b)));
+        return EXIT_SUCCESS;
+ERROR:
+        gfree(ga);
+        gfree(blah);
+
+        return EXIT_FAILURE;
+
+}
