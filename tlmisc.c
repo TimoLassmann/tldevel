478f9e157461219615b1b795bb82e0e585a3cc6c
Timo Lassmann <timo.lassmann@telethonkids.org.au>
2020-05-09 09:01:41 -0400

    make cmd line

--- tlmisc.c
+++ tlmisc.c
@@ -1,8 +1,10 @@
 #include <sys/stat.h>
 
+#include <string.h>
+
 #define TLMISC_IMPORT
 #include "tlmisc.h"
-
+#include "tldevel.h"
 
 int my_file_exists(const char* name)
 {
@@ -16,4 +18,39 @@ int my_file_exists(const char* name)
                 ret++;
         }
         return ret;
+}
+
+int make_cmd_line(char** command, const int argc,char* const argv[])
+{
+        char* cmd = NULL;
+        int i,j,c;
+
+        RUN(galloc(cmd,16384));
+
+        c = 0;
+        for(i =0 ; i < argc;i++){
+                for(j = 0; j < (int) strlen(argv[i]);j++){
+                        if(c == 16384-1){
+                                break;
+                        }
+                        cmd[c] = argv[i][j];
+                        c++;
+
+                }
+                if(c == 16384-1){
+                        break;
+                }
+                cmd[c] = ' ';
+                c++;
+
+
+        }
+        cmd[c] = 0;
+        *command = cmd;
+        return OK;
+ERROR:
+        if(cmd){
+                gfree(cmd);
+        }
+        return FAIL;
 }
